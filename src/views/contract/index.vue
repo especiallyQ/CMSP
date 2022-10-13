937171cc8b706c284d3d09e5340b9f4caa7f37ea
chengjiang_09 <chengjiang_09@163.com>
2022-10-13 18:04:10 +0800

    修改了模板Card，将不再自动处理对应的i18n字段

--- src/views/contract/index.vue
+++ src/views/contract/index.vue
@@ -3,46 +3,23 @@
     <ContentHead :headTitle="$t('title.contractTemplate')" />
     <div class="card-template-container">
       <el-card class="box-card">
-        <div class="clearfix">
-          <div class="clearfix-container-left">
-            <input
-              type="text"
-              class="search searchCreater"
-              placeholder="创建者搜索"
-            />
-            <input
-              type="text"
-              placeholder="合约模板搜索"
-              class="search searchContractTemplate"
-            />
-            <el-button class="searchButton" icon="el-icon-search"></el-button>
-          </div>
-          <div class="clearfix-container-right">
-            <el-button-group>
-              <el-button
-                type="primary"
-                class="iconBtn"
-                :class="{ action: changeAction }"
-                @click="changeAction = false"
-              >
-                <i class="cmsp-icon-caidan"></i>
-              </el-button>
-              <el-button
-                type="primary"
-                class="iconBtn"
-                :class="{ action: !changeAction }"
-                @click="changeAction = true"
-              >
-                <i class="el-icon-menu"></i>
-              </el-button>
-            </el-button-group>
-          </div>
-        </div>
+        <ContractHeader />
         <ul class="contract-cartList">
-          <TemplateCard v-for="(num, index) in 5" :key="index" @click="test" @mouseenter="test" :propTitleIconColor="getRgb()">
-            <BottomNav v-for="(num, index) in 1" :key="index" @click="test"/>
+          <TemplateCard
+            v-for="(obj, index) in contractTemplateList.newDataList"
+            :key="index"
+            :propTitleIconColor="getRgb()"
+            :propTitle="obj.templateName"
+            :propDetilDate="obj.data"
+          >
+            <BottomNav v-for="(num, index) in 1" :key="index" />
           </TemplateCard>
         </ul>
+        <PaginationView
+          @changeHandleCurrent="changeHandleCurrent"
+          @changeHandleSize="changeHandleSize"
+          :propTotalNum="contractTemplateList.totalCount"
+        />
       </el-card>
     </div>
   </div>
@@ -53,7 +30,9 @@ import ContentHead from "@/components/contentHead.vue";
 import dispatchKeys from "@/util/storeKeys/contractTemplate/dispatchKeys";
 import storageKeys from "@/util/storageKeys/contractTemplateStorageKey";
 import TemplateCard from "@/components/templateCard/index.vue";
-import BottomNav from '@/components/templateCard/components/bottomNav.vue';
+import BottomNav from "@/components/templateCard/components/bottomNav.vue";
+import ContractHeader from "./components/contractHeader.vue";
+import PaginationView from "@/components/paginationView.vue";
 import { rgb } from "@/util/util";
 
 export default {
@@ -61,17 +40,88 @@ export default {
   components: {
     ContentHead,
     TemplateCard,
-    BottomNav
+    BottomNav,
+    ContractHeader,
+    PaginationView,
   },
   data: () => {
     return {
-      isCollapse: true,
-      changeAction: true,
+      pageSize: 1,
+      showDataList: {
+        creator: "templateCreatorShort",
+        description: "templateDescriptionShort",
+        descriptionEn: "templateDescriptionShort",
+        versionCount: "templateVersionCount",
+        templateName: "templateName",
+        templateNameEn: "templateName",
+        visibility: "templateVisibilityShort",
+      },
+      visibilityList: {
+        0:"所有人可见",
+        1:"群组内可见",
+        2:"仅自己可见"
+      }
     };
   },
+  computed: {
+    contractTemplateList: {
+      get() {
+        let { data, totalCount } =
+          this.$store.state.contractTemplate.contractTemplateData.data;
+
+        let dataLen = data.length;
+        let newDataList = [];
+
+        for (let i = 0; i < dataLen; i++) {
+          let obj = {
+            templateName: "",
+            data: {},
+          };
+          obj.templateName = this.$t("text.noData");
+          for (let key in data[i]) {
+            if (this.showDataList[key]) {
+              let i18nName = this.$t(`table.${this.showDataList[key]}`);
+
+              if (data[i][key] !== null) {
+                if (this.showDataList[key] !== "templateName") {
+                  if(this.showDataList[key] !== "templateVisibilityShort") {
+                    obj.data[i18nName] = data[i][key];
+                  }else {
+                    obj.data[i18nName] = this.visibilityList[data[i][key]];
+                  }
+                } else {
+                  obj.templateName = data[i][key];
+                }
+              } else {
+                if (this.showDataList[key] !== "templateName") {
+                  if (
+                    !obj.data[i18nName] &&
+                    i18nName === this.$t(`table.templateDescriptionShort`)
+                  ) {
+                    obj.data[i18nName] = this.$t("text.noRemark");
+                  } else if (!obj.data[i18nName]) {
+                    obj.data[i18nName] = this.$t("text.noData");
+                  }
+                }
+              }
+            }
+          }
+          newDataList.push(obj);
+        }
+
+        return {
+          newDataList,
+          totalCount,
+        };
+      },
+      set(pageSize) {
+        this.pageSize = pageSize;
+      },
+    },
+  },
   methods: {
-    getRgb(){
-      return rgb()
+    getRgb() {
+      return rgb();
     },
     getContractTemplateList(data) {
       this.$store.dispatch(dispatchKeys.CONTRACT_TEMPLATE_LIST);
@@ -91,9 +141,12 @@ export default {
       //默认返回true，表示当前数据有误，将重新请求新数据
       return true;
     },
-    test(){
-      console.log(123)
-    }
+    changeHandleCurrent(val) {
+      console.log(val);
+    },
+    changeHandleSize(val) {
+      this.pageSize = val;
+    },
   },
   created() {
     // 过期更新，有效期60秒
@@ -120,120 +173,7 @@ export default {
 
 .card-template-container .contract-cartList {
   display: flex;
-  justify-content: space-between;
+  justify-content: flex-start;
   flex-wrap: wrap;
-}
-
-.card-template-container .clearfix {
-  position: relative;
-  height: 81px;
-}
-
-.card-template-container .clearfix .clearfix-container-left {
-  position: absolute;
-  left: 0;
-  top: 50%;
-  transform: translate(0, -60%);
-  height: 36px;
-  width: 340px;
-  display: flex;
-}
-
-.card-template-container .clearfix .clearfix-container-left .search {
-  display: inline-block;
-  border: 1px solid #eaedf3;
-  padding: 0 15px 0 15px;
-  box-sizing: border-box;
-  width: 100%;
-  color: #000;
-}
-
-.card-template-container .clearfix .clearfix-container-left .search:focus {
-  outline: none;
-}
-
-.card-template-container .clearfix .clearfix-container-left input::placeholder {
-  color: #c3c5c9;
-}
-
-.card-template-container
-  .clearfix
-  .clearfix-container-left
-  input::-moz-placeholder {
-  color: #c3c5c9;
-}
-
-.card-template-container
-  .clearfix
-  .clearfix-container-left
-  input::-webkit-input-placeholder {
-  color: #c3c5c9;
-}
-
-.card-template-container
-  .clearfix
-  .clearfix-container-left
-  .search:nth-child(2) {
-  border-left: 0px;
-}
-
-.card-template-container .clearfix .clearfix-container-left .searchCreater {
-  flex: 6;
-  border-top-right-radius: 0%;
-  border-bottom-right-radius: 0%;
-}
-
-.card-template-container
-  .clearfix
-  .clearfix-container-left
-  .searchContractTemplate {
-  flex: 14;
-  border-radius: 0%;
-}
-
-.card-template-container .clearfix .clearfix-container-left .searchButton {
-  flex: 1;
-  color: #ffffff;
-  background-color: #4093ff;
-  border-top-left-radius: 0%;
-  border-bottom-left-radius: 0%;
-}
-
-.card-template-container .clearfix .clearfix-container-right {
-  position: absolute;
-  right: 0;
-  top: 50%;
-  transform: translate(0, -60%);
-  height: 36px;
-}
-
-.card-template-container .clearfix .clearfix-container-right .iconBtn {
-  position: relative;
-  font-size: 15px !important;
-  width: 50px;
-  height: 36px;
-  background-color: #409eff !important;
-  border-color: #409eff !important;
-}
-
-.card-template-container .clearfix .clearfix-container-right .action {
-  background-color: #909399 !important;
-  border-color: #909399 !important;
-}
-
-.card-template-container .clearfix .clearfix-container-right .iconBtn:hover {
-  background-color: #bec1c7 !important;
-  border-color: #cacdd4 !important;
-}
-
-.card-template-container .clearfix .clearfix-container-right .cmsp-icon-caidan {
-  font-size: 16px !important;
-}
-
-.card-template-container .clearfix .clearfix-container-right .iconBtn i {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
 }
 </style>
